2444f3f01539d07070c3e6ee288d8585d908448a
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-24 14:02:53 -0400

    Fix API endpoint route issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v2 do
-      get "greetings/random", to "greetings#random_greeting"
+      get "greetings/random", to: "greetings#random_greeting"
     end
   end
 
